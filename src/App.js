005a8e20eb6354a967f5f19d7051ab43662edcf5
thilakmurthy <71584170+thilakmurthy@users.noreply.github.com>
2024-05-09 12:42:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import KanbanBoard from "./KanbanBoard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Agile Board</h1>
+      <KanbanBoard />
+    </div>
+  );
+}
